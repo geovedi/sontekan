4e50dcc60710cdf896d71ddd57e48ffb66e00ad5
Jim Geovedi <jim@geovedi.com>
2015-02-03 01:53:53 +0700

    Create dbpedia-article-map.py

---
+++ dbpedia-article-map.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import logging
+logging.basicConfig(format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+
+
+import plyvel
+import pickle
+import ujson as json
+import regex as re
+from collections import defaultdict
+from unidecode import unidecode
+from rdflib.plugins.parsers.ntriples import NTriplesParser, Sink
+
+wordset = set(pickle.load(open('./wordset.p')))
+
+
+def norm(text):
+    return '_'.join(re.sub(r'[\p{P}\p{S}]+', ' ', unidecode(text).lower()).split())
+
+
+class Label(Sink):
+    def __init__(self):
+        self.labels = plyvel.DB('./labels', create_if_missing=True)
+
+    def triple(self, s, p, o):
+        k = s.encode('utf-8')
+        v = norm(o).encode('utf-8')
+        self.labels.put(k, v)
+        logging.info('labels: {0} => {1}'.format(k, v))
+
+
+class Category(Sink):
+    def __init__(self):
+        self.categories = plyvel.DB('./categories', create_if_missing=True)
+
+    def triple(self, s, p, o):
+        k = o.encode('utf-8')
+        v = norm(s).encode('utf-8') # norm requires unicode input
+        logging.info((k, v))
+        data = self.categories.get(k)
+        if data:
+            data = set(json.loads(data))
+        else:
+            data = set()
+        if v in wordset:
+            data.add(v)
+            self.categories.put(k, json.dumps(list(v)))
+            logging.info('categories: {0} => {1}'.format(k, v))
+
+
+if __name__ == '__main__':
+    
+    labels = NTriplesParser(sink=Label())
+    categories = NTriplesParser(sink=Category())
+
+
+    for filename in ['labels_en.nt', 'labels_en_uris_id.nt', 'category_labels_en.nt', 'category_labels_en_uris_id.nt']:
+        logging.info('labels: processing: {0}'.format(filename))
+        for line in open(filename):
+            labels.parsestring(line)
+
+    for filename in ['article_categories_en.nt', 'article_categories_en_uris_id.nt']:
+        logging.info('categories: processing: {0}'.format(filename))
+        for line in open(filename):
+            categories.parsestring(line)
+
