586e9113d21947782cf1f3a49719c6d7dba5fd69
Jim Geovedi <jim@geovedi.com>
2015-02-03 02:03:13 +0700

    wordset as param

--- dbpedia-article-map.py
+++ dbpedia-article-map.py
@@ -13,8 +13,6 @@ from collections import defaultdict
 from unidecode import unidecode
 from rdflib.plugins.parsers.ntriples import NTriplesParser, Sink
 
-wordset = set(pickle.load(open('./wordset.p')))
-
 
 def norm(text):
     return '_'.join(re.sub(r'[\p{P}\p{S}]+', ' ', unidecode(text).lower()).split())
@@ -32,8 +30,9 @@ class Label(Sink):
 
 
 class Category(Sink):
-    def __init__(self, loc):
+    def __init__(self, loc, wordset=set()):
         self.db = plyvel.DB(loc, create_if_missing=True)
+        self.wordset = wordset
 
     def triple(self, s, p, o):
         k = o.encode('utf-8')
@@ -44,16 +43,17 @@ class Category(Sink):
             data = set(json.loads(data))
         else:
             data = set()
-        if v in wordset:
+        if v in self.wordset:
             data.add(v)
             self.db.put(k, json.dumps(list(v)))
             logging.info('categories: {0} => {1}'.format(k, v))
 
 
 if __name__ == '__main__':
-    
+
+    wordset = set(pickle.load(open('./wordset.p')))
     labels = NTriplesParser(sink=Label('./labels'))
-    categories = NTriplesParser(sink=Category('./categories'))
+    categories = NTriplesParser(sink=Category('./categories', wordset))
 
 
     for filename in ['./labels_en.nt', './labels_en_uris_id.nt', './category_labels_en.nt', './category_labels_en_uris_id.nt']:
