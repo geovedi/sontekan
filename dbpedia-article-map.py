95c913015ac90f54691e6b2c69d43e550610e483
Jim Geovedi <jim@geovedi.com>
2015-02-03 19:16:32 +0700

    Update dbpedia-article-map.py

--- dbpedia-article-map.py
+++ dbpedia-article-map.py
@@ -9,6 +9,7 @@ import plyvel
 import pickle
 import ujson as json
 import regex as re
+from urllib import unquote
 from collections import defaultdict
 from unidecode import unidecode
 from rdflib.plugins.parsers.ntriples import NTriplesParser, Sink
@@ -50,16 +51,23 @@ class Category(Sink):
 
     def triple(self, s, p, o):
         k = o.encode('utf-8')
-        v = s.encode('utf-8')
+        s = s.encode('utf-8')
 
         if self.labels:
-            v = self.labels.get(v, default=v)
+            v = self.labels.get(s, default=set())
 
-        if v in self.wordset:
-            data = self.db.get(k, default=set())
-            if data:
-                data = set(json.loads(data))
-            data.add(v)
+        if v:
+            v = set(json.loads(v))
+
+        data = self.db.get(k, default=set())
+        if data:
+            data = set(json.loads(data))
+
+        for w in v:
+            if w in self.wordset:
+                data.add(w)
+
+        if data:
             self.db.put(k, json.dumps(list(data)))
             logging.info('categories: {0} => {1}'.format(k, v))
 
@@ -85,6 +93,14 @@ if __name__ == '__main__':
         logging.info('categories: processing: {0}'.format(filename))
         for line in open(filename):
             categories.parsestring(line)
+
+    with open('categories.txt', 'w') as out:
+        for k, v in categories.sink.db:
+            v = json.loads(v)
+            if len(v) >= 3 and not 'establishments' in k \
+                and not k.endswith('births') \
+                and not k.endswith('deaths'):
+                out.write('{0}\t{1}\n'.format(unquote(k), ','.join(v)))
 
     labels.sink.db.close()
     categories.sink.db.close()
