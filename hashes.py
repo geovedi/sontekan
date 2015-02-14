5b783602dbf90da54a34b187e099c49006705b15
Jim Geovedi <jim@geovedi.com>
2015-02-14 23:12:23 +0700

    Update hashes.py

--- hashes.py
+++ hashes.py
@@ -5,3 +5,10 @@ def djbhash(a):
         t = (h * 33) & 0xffffffffL 
         h = t ^ i 
     return h 
+
+def fnvhash(a):
+    h = 2166136261
+    for i in a:
+        t = (h * 16777619) & 0xffffffffL
+        h = t ^ i
+    return h
