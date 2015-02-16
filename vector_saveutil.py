aae349d00b49be9570d2fa1b5883d40ace28a1dd
Jim Geovedi <jim@geovedi.com>
2015-02-17 00:05:43 +0700

    Update vector_saveutil.py

--- vector_saveutil.py
+++ vector_saveutil.py
@@ -3,19 +3,31 @@ import cPickle as pickle
 import numpy as np
 import scipy.sparse as sp
 
-# Corpus
 # XXX: Store and load CSR from HDF5?
 
-## Save
+## Corpus: Save
 f = h5py.File('corpus.h5')
 for par in ('data', 'row', 'col', 'shape'):
     f.create_dataset(par, data=np.array(getattr(corpus.matrix, par)))
-
 corpus.__dict__.pop('matrix')
 pickle.dump(corpus, open('corpus.p', 'wb'), protocol=2)
 
-## Load
+## Corpus: Load
 corpus = pickle.load(open('corpus.p'))
 f = h5py.File('corpus.h5')
 corpus.matrix = sp.coo_matrix((f['data'], (f['row'], f['col'])),
                               shape=f['shape'], dtype=np.float64)
+
+
+## Model: Save
+f = h5py.File('corpus.h5')
+for field in ['word_vectors', 'word_biases', 'vectors_sum_gradients', 'biases_sum_gradients']:
+    f.create_dataset(field, data=model.__dict__[field])
+    model.__dict__.pop(field)
+pickle.dump(model, open('corpus.p', 'wb'), protocol=2)
+
+## Model: Load
+model = pickle.load(open('model.p'))
+f = h5py.File('corpus.h5')
+for field in ['word_vectors', 'word_biases', 'vectors_sum_gradients', 'biases_sum_gradients']:
+    model.__dict__[field] = f[field]
