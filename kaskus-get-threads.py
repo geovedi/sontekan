55d56ca7716b0e67280f3428cf5ad839ecbb35b8
Jim Geovedi <jim@geovedi.com>
2015-12-14 06:13:33 +0700

    Update kaskus-get-threads.py

--- kaskus-get-threads.py
+++ kaskus-get-threads.py
@@ -2,8 +2,12 @@
 
 from __future__ import unicode_literals
 
-import lxml.html
+import grequests
 import plac
+import itertools
+import io
+import regex as re
+import lxml.html
 import json
 
 import logging
@@ -12,10 +16,19 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO)
 
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
+HEADERS = {'User-Agent': USER_AGENT}
 BASE_URL = 'http://www.kaskus.co.id'
 
-def main(input_fname):
-    html = lxml.html.parse(input_fname)
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+
+def parse(result):
+    if not result.text: return
+    html = lxml.html.document_fromstring(result.text)
     for div_post in html.xpath('//div[@class="post"]'):
         data = {}
         link_thread = div_post.xpath('div[@class="post-title"]/a')[0]
@@ -27,6 +40,14 @@ def main(input_fname):
         else:
             data['last_page'] = 1
         print json.dumps(data)
+
+
+def main(forum_index, start, end):
+    for chunk in grouper(xrange(int(start), int(end)), 100):
+        requests = (grequests.get(forum_index + '/' + str(page_id), headers=HEADERS) for page_id in chunk)
+        results = grequests.map(requests)
+        map(parse, results)
+
 
 if __name__ == '__main__':
     plac.call(main)
