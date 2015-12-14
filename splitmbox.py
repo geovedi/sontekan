3aff6feeebe6af48297b2d66a1554303bee6e1af
Jim Geovedi <jim@geovedi.com>
2015-12-15 00:31:02 +0700

    Create splitmbox.py

---
+++ splitmbox.py
@@ -0,0 +1,29 @@
+import mailbox
+import plac
+import logging
+logging.basicConfig(
+    format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
+
+
+def main(mbox_file, output_dir):
+    mbox = mailbox.UnixMailbox(open(mbox_file))
+    counter = 0
+    while True:
+        msg = mbox.next()
+        if not msg:
+            break
+        output_fname = output_dir + '/' + str(counter) + '.mbox'
+        with open(output_fname, 'w') as out:
+            for hdr in msg.headers:
+                out.write(hdr)
+            out.write('\n')
+            for bln in msg.fp.readlines():
+                out.write(bln)
+        logging.info('Saved {0}'.format(output_fname))
+        counter += 1
+
+
+if __name__ == '__main__':
+    plac.call(main)
