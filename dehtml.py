3767a2b184d1fc9906a10d7bba80d1f4ab7c166f
Jim Geovedi <jim@geovedi.com>
2016-03-03 11:58:35 +0700

    Create dehtml.py

---
+++ dehtml.py
@@ -0,0 +1,39 @@
+# http://stackoverflow.com/a/3987802
+
+import re
+from HTMLParser import HTMLParser
+
+class DeHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.__text = []
+
+    def handle_data(self, data):
+        text = data.strip()
+        if len(text) > 0:
+            text = re.sub('[ \t\r\n]+', ' ', text)
+            self.__text.append(text + ' ')
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.__text.append('\n\n')
+        elif tag == 'br':
+            self.__text.append('\n')
+
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            self.__text.append('\n\n')
+
+    def text(self):
+        return ''.join(self.__text).strip()
+
+
+def dehtml(text):
+    try:
+        parser = DeHTMLParser()
+        parser.feed(text)
+        parser.close()
+        return parser.text()
+    except:
+        return text
+
