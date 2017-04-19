a43881d5a3cc93169f0ffc9c82703e4506aa4de2
Jim Geovedi <jim@geovedi.com>
2017-04-19 15:12:48 +0700

    Update gtrans-fix.pl

--- gtrans-fix.pl
+++ gtrans-fix.pl
@@ -4,20 +4,20 @@ use warnings;
 use strict;
 
 while(<STDIN>) {
-  chop;
+    chop;
 
-  # avoid general madness
-  s/[\000-\037]//g;
-  s/\s+/ /g;
-  s/(£|$|Rp)(\d+)/$1 $2/g;
-  s/, "(kata|ujar|ungkap|tutur)/," $1/g;
-  s/, "(\w+) (me\w+|said|told)/," $1 $2/g;
-  s/ +\.$/./g;
-  s/ +\.\.\./.../g;
-  s/ +([,:;\.]|'[,s]) +/$1 /g;
-  s/ +([\/]) +/\//g;
-  s/^ //g;
-  s/ $//g;
+    # avoid general madness
+    s/[\000-\037]//g;
+    s/\s+/ /g;
+    s/(£|€|$|Rp)(\d+)/$1 $2/g;
+    s/, "(kata|ujar|ungkap|tutur)/," $1/g;
+    s/, "(\w+) (me\w+|said|told)/," $1 $2/g;
+    s/ +\.$/./g;
+    s/ +\.\.\./.../g;
+    s/ +([,:;\.]|'[,s]) +/$1 /g;
+    s/ +([\/]) +/\//g;
+    s/^ //g;
+    s/ $//g;
 
-  print $_."\n";
+    print $_."\n";
 }
